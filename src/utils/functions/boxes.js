ad74b8918ae322532c30cc39598b76d744622d7e
femellone <fernandomellone2@gmail.com>
2021-11-21 05:57:56 -0300

    adding a better way to handle styles

--- src/utils/functions/boxes.js
+++ src/utils/functions/boxes.js
@@ -1,3 +1,8 @@
+// classes
+import { classes } from '../classes/boxes'
+
+
+// svelteMoveable
 import { makeMoveable } from '../../libs/svelteMovaeble/makeElementMoveable';
 // import { makeResizable } from '../../libs/svelteMovaeble/makeElementResizable';
 
@@ -6,18 +11,19 @@ export function addSquare(event) {
     var square = document.createElement("div");
 
     // Styling the element
-    square.style.display = "flex";
-    square.style.justifyContent = "flex-start";
-    square.style.alignItems = "start";
-    square.style.width = "100px";
-    square.style.height = "100px";
-    square.style.position = "fixed";
+    // square.style.display = "flex";
+    // square.style.justifyContent = "flex-start";
+    // square.style.alignItems = "start";
+    // square.style.width = "100px";
+    // square.style.height = "100px";
+    // square.style.position = "fixed";
     square.style.top = `${event.clientY}px`;
     square.style.left = `${event.clientX}px`;
-    square.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    square.style.color = "#FFFFFF";
-    square.style.zIndex = "100000000000000000000000000000000000000000000000";
-    square.style.border = "2px solid #FF0000";
+    square.classList.add(classes.square);
+    // square.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    // square.style.color = "#FFFFFF";
+    // square.style.zIndex = "100000000000000000000000000000000000000000000000";
+    // square.style.border = "2px solid #FF0000";
 
     square.addEventListener("click", function (event) {
         event.stopPropagation();
@@ -30,7 +36,6 @@ export function addSquare(event) {
     });
 
     // makeResizable(event);
-
 
     document.body.append(square);
 }
